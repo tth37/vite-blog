1b8e94f9d7683dd50be507d13099f990b19b4fbc
田昊冬 <xgpsthd0902@outlook.com>
2023-02-17 18:21:16 +0800

    Modify Hash history

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import { createPinia } from 'pinia'
 import './index.css'
 import Home from './Home.vue'
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
