3981d97148c3ad27da9f9d89f241ad878356cbc3
tth37 <xgpsthd0902@outlook.com>
2023-02-18 14:49:55 +0800

    Modify markdown ul style

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import { createPinia } from 'pinia'
-import './index.css'
 import Home from './Home.vue'
 import App from './App.vue'
 import { useConfigStore } from './stores/config'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faLocationDot, faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+import './index.css'
 import 'katex/dist/katex.min.css';
+import 'highlight.js/styles/github.css';
+import 'github-markdown-css/github-markdown-light.css';
+
 
 library.add(faLocationDot)
 
